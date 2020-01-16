ae7b8c17753d6f2711266433761cc92ac9a84bff
jujix <18301246+jujix@users.noreply.github.com>
2020-01-16 19:04:03 +0200

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import  {  Switch, Route } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop.jsx';
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop.jsx";
+import Header from "./components/header/header.jsx";
 
 function App() {
   return (
     <div>
-    <Switch>
-    <Route exact path='/' component={HomePage} />
-    <Route path='/shop/hats' component={ShopPage} />
-    </Switch>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
