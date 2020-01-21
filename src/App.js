785f03f49537f88189b3cf17052c3cb4f9a5f790
jujix <18301246+jujix@users.noreply.github.com>
2020-01-21 17:33:02 +0200

    Add Form and Button components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
-import ShopPage from "./pages/shop/shop.jsx";
-import Header from "./components/header/header.jsx";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop/hats" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/custom-button/custom-button.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react';
+
+import './custom-button.scss';
+
+const CustomButton = ({ children, ...otherProps }) => (
+    <button className='custom-button' {...otherProps}>
+    {children}
+    </button>
+);
+
+export default CustomButton;
