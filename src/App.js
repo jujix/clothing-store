46f821a9b68c5abb0a3068f9ae0d244a25285165
jujix <18301246+jujix@users.noreply.github.com>
2020-01-24 19:19:17 +0200

    Integrate firebase to application and add sign in and out with google

--- src/App.js
+++ src/App.js
@@ -6,18 +6,43 @@ import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import { auth } from './firebase/firebase.utils'; 
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
     <div>
-      <Header />
+      <Header currentUser={this.state.currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop/hats" component={ShopPage} />
         <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
