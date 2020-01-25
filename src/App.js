0e3829cb2617b83ed932fd5c0942c7d1d8af857d
jujix <18301246+jujix@users.noreply.github.com>
2020-01-25 10:06:10 +0200

    Store and get user data from db

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import { auth } from './firebase/firebase.utils'; 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'; 
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+if (userAuth) {
+  const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+  userRef.onSnapshot(snapShot => {
+    this.setState({
+      currentUser: {
+        id: snapShot.id,
+        ...snapShot.data()
+      }
+    });
+  } );
+}                  
+this.setState({ currentUser: userAuth });
     });
   }
 
