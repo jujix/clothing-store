f1276a77ec2d3d3a08552fbccd69604717450e86
jujix <18301246+jujix@users.noreply.github.com>
2020-02-19 19:41:33 +0200

    Fix the showing of total price in Stripe

--- src/components/stripe-button/stripe-button.jsx
+++ src/components/stripe-button/stripe-button.jsx
@@ -17,7 +17,7 @@ const StripeCheckoutButton = ({ price }) => {
       billingAddress
       shippingAddress
       image="https://sendeyo.com/up/d/f3eb2117da"
-      description="{`Your total is $${price}`}"
+      description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
       token={onToken}
