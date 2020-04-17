4acb615eb9fb8bdc34178d52ad47144ed58f8972
jujix <18301246+jujix@users.noreply.github.com>
2020-04-17 18:49:51 +0300

    Wrap components inside of ErrorBoundary

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { createStructuredSelector } from "reselect";
 
 import Header from "./components/header/header";
 import Spinner from "./components/spinner/spinner";
+import ErrorBoundary from "./components/error-boundary/error-boundary";
 
 import { GlobalStyle } from "./global.styles";
 
@@ -28,18 +29,20 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle></GlobalStyle>
       <Header />
       <Switch>
-        <Suspense fallback={<Spinner></Spinner>}>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-            }
-          />
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner></Spinner>}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route
+              exact
+              path="/signin"
+              render={() =>
+                currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+              }
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
