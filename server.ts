4732998f91429c6293846b75eb586f818c55a1a8
jujix <18301246+jujix@users.noreply.github.com>
2022-03-26 13:05:03 +0200

    Add rateLimiter to all routes

--- server.ts
+++ server.ts
@@ -9,12 +9,19 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const RateLimit = require('express-rate-limit');
+const limiter = new RateLimit({
+  windowMs: 1*60*1000,
+  max: 5
+});
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(limiter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(compression());
